6c56c5f3b2d47fe9d8da032578a546dd99b06de7
Alejandro Lopez <alejandro.lopez@spotbros.com>
2013-08-17 14:17:48 +0200

    no message

--- buscapp.php
+++ buscapp.php
@@ -44,9 +44,9 @@ class BuscApp extends SBPersistentApp
 				shuffle($results);
 				foreach ($results as $result)
 				{
-					if(strlen($result>32))
+					if(strlen($result)>32)
 					{
-						$this->replyOrFalse("Añado ".substr($result, -32));
+						//$this->replyOrFalse("Añado ".substr($result, -32));
 						$this->_SBAttachments->addAttachmentRef(substr($result, -32));
 					}
 				}
