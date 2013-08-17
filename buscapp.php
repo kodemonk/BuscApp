d79fcd0633d5d12890a74c5314b69fe20c2b7899
Alejandro Lopez <alejandro.lopez@spotbros.com>
2013-08-17 12:44:13 +0200

    no message

--- buscapp.php
+++ buscapp.php
@@ -23,7 +23,7 @@ class BuscApp extends SBPersistentApp
 			$this->_SBAttachments->addAttachmentRef($aRef);
 		}
 		
-		$this->sendTextMessageOrFalse("Esto es lo que has adjuntado");
+		$this->replyOrFalse("Esto es lo que has adjuntado");
 	}
 }
 $buscapp=new BuscApp("LNU1S95","4fde8e99501000e58d06c7c9e7583d5b588f07b94a2c8d91e9f785b55da511fe");
